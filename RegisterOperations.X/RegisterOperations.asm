17ee1d40cfdf4bc0a0dc0fb2fbce243bd827dc03
Alvaro Abarca <abargdev@gmail.com>
2023-10-01 16:51:07 -0600

    tarea2

---
+++ RegisterOperations.X/RegisterOperations.asm
@@ -0,0 +1,120 @@
+;Programa que realiza operaciones con registros y WREG, en los valores 
+    
+    #include <p18f4550.inc>
+    ORG	.0
+    
+#DEFINE	VIDA	.17
+#DEFINE	VIDB	.29
+#DEFINE	DATOA       .19
+#DEFINE	DATOB	.3
+
+    CBLOCK	.50
+	RSUMA
+	RAND
+	RCOMF
+	RDEC
+	RIOR
+	RMULHI
+	RMULLW
+	RNEG
+	RSET
+	RSUB
+	RXOR
+	RSWAP
+    ENDC
+      
+SETTINGS
+    CLRF    WREG
+    CLRF    DATOA
+    CLRF    DATOB
+    
+MAIN
+    MOVLW	    VIDA
+    MOVWF	    DATOA
+    MOVLW	    VIDB
+    MOVWF	    DATOB
+    
+SUMA
+    MOVF    DATOA,W
+    
+    ADDWF   DATOB,W
+    MOVFF	  WREG,RSUMA
+    
+    INCF	  DATOB,F
+    
+AND
+    MOVF    DATOA,W
+    
+    ANDWF   DATOB,W
+    MOVFF   WREG,RAND
+    
+    INCF DATOB,F
+    
+COMPLEMENT 
+    COMF    DATOA,W
+    MOVFF   WREG,RCOMF
+    
+    INCF DATOB,F
+    
+DECREMENT
+    DECF    DATOA,W
+    MOVFF   WREG,RDEC
+    
+    INCF DATOB,F
+    
+INCLUSIVEOR
+    MOVF	DATOA,W
+   
+    IORWF    DATOB,W
+    MOVFF    WREG,RIOR
+   
+    INCF	DATOB,F
+   
+MULTIPLY
+    MOVF	DATOA,W
+   
+    MULWF    DATOB
+    MOVFF    PRODH,RMULHI
+    MOVFF    PRODL,RMULLW
+   
+    INCF	DATOB,F
+   
+NEGATE
+    NEGF	DATOA,W
+    MOVFF    WREG,RNEG
+   
+    INCF	DATOB,F
+   
+SETFILE
+    SETF	DATOA,W
+    MOVFF    WREG,RSET
+   
+    INCF	DATOB,F
+   
+SUBNOBORROW
+    MOVF	DATOB,W
+   
+    SUBWF    DATOA,W
+    MOVFF    WREG,RSUB
+   
+    INCF	DATOB,F
+   
+SWAP
+    SWAPF    DATOA,W
+    MOVFF    WREG,RSWAP
+   
+    INCF	DATOB,F
+   
+EXCLUSIVEOR
+    MOVF	DATOA,W
+   
+    XORWF    DATOB,W
+    MOVFF    WREG,RXOR
+   
+    INCF	    DATOB,F
+   
+FINISH
+    GOTO    MAIN
+    END
+
+
---
+++ RegisterOperations.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=RegisterOperations.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/RegisterOperations.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=registeroperations.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/registeroperations.x.tar
